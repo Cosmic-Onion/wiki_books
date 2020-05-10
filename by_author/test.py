f6652218e86c72fa10425bf73b9e988459d5224c
cosmic-onion <marmaladeking@gmail.com>
2020-05-10 20:24:30 +0100

    xpath

--- by_author/test.py
+++ by_author/test.py
@@ -32,6 +32,34 @@ for nationality in writers:
        linkList.append(nextPage)
        addNationalities(nextPage)
 
+'''
 with open("linkList0.txt","w") as txtFile:
     for item in linkList:
         txtFile.write("%s\n" % item)
+'''
+writerDict = {}
+
+for link in linkList:
+    writerList = []
+    lupine = lxml.html.fromstring(req.get(link).text)
+
+    #TODO: add if statement to check if this even exists
+
+    for writer in lupine.xpath("/html/body/div[3]/div[3]/div[4]/div/div/ul/li"):
+        writerLinks = writer.xpath("a")
+        if writerLinks == []:
+            continue
+        else:
+            writerName = writerLinks[0].text_content()
+            for writerPage in writer.iterlinks():
+                writerURL = writerPage[2]
+                break
+
+            temp = {writerName : writerURL}
+            writerList.append(temp) #improve memory usage
+
+    writerDict[link] = []
+    writerDict[link].append(writerList)
+
+with open("../json/author5.json", "w", encoding="utf8") as outfile:
+    json.dump(writerDict, outfile)
